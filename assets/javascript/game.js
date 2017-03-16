e7a07bacd2f22ca02107a4ebd597aacba22dcac5
Jake Beaube <Jake Beaube>
2017-03-15 22:25:59 -0400

    1st draft no music

---
+++ assets/javascript/game.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+
+	skulls = ['assets/images/red.png','assets/images/purple.png','assets/images/black.png','assets/images/green.png'];
+
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+	$('#win').text(wins);
+	$('#loss').text(losses);
+	
+	newskulls();
+	newGame();
+
+	function newskulls () {
+		var numbers = []
+			while(numbers.length < 4){
+			  var randomnumber = Math.ceil(Math.random()*12)
+			  var found = false;
+			  for (var i=0; i< numbers.length; i++){
+				if (numbers[i] == randomnumber){
+					found = true; break
+				}
+			  }
+			  if(!found)numbers[numbers.length]=randomnumber;
+			}
+		console.log(numbers);		
+
+		for (i = 0; i < numbers.length; i++) {
+			var imageSkull = $('<img>');
+			imageSkull.attr('data-num', numbers[i]);
+			imageSkull.attr('src', skulls[i]);
+			imageSkull.attr('alt', 'skulls');
+			imageSkull.addClass('skullImage')
+			$('#skulls').append(imageSkull);
+		}
+	}
+
+	function newGame() {
+
+		counter = 0;
+		$('#yourScore').text(counter);
+
+		function randomIntFromInterval(min,max){
+		   	return Math.floor(Math.random()*(max-min+1)+min);
+			}
+
+		var numberToGuess = randomIntFromInterval(19,120);
+
+		$('.value').text(numberToGuess);
+
+
+		$('.skullImage').on('click', function(){
+		    counter = counter + parseInt($(this).data('num'));
+		   
+		    $('#yourScore').text(counter);
+
+		    if (counter == numberToGuess){
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      console.log(wins)
+		      $('#skulls').empty();
+		      newskulls();
+		      newGame();
+		        
+		    } else if ( counter > numberToGuess){
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        console.log(losses)
+		        $('#skulls').empty();
+		        newskulls();
+		        newGame();
+		    }
+		});
+	}
+
+});
